fa971129c6c3473f4100ee862a8d3e24969d022a
MikeBLambert <mlamb128@gmail.com>
2018-01-20 09:45:34 -0800

    add content to readme file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 
+//Business Logic
+//A string output is determined by adding up the relevant variables.
+function vacationDecision (bigCities, introverted, singing, ocean) {
+  if (bigCities + introverted + singing===111) {
+    return "Lima"
+  } else if (bigCities + introverted + singing===211) {
+    return "Tokyo"
+  } else if (bigCities + introverted + ocean===1021) {
+    return "Rio de Janeiro"
+  } else if (bigCities + introverted + ocean===2021) {
+    return "Seattle"
+  } else if (bigCities + introverted + singing===112) {
+    return "Cusco"
+  } else if (bigCities + introverted + singing===212) {
+    return "Karuizawa"
+  } else if (bigCities + introverted + ocean===1022) {
+    return "Ilha Grande"
+  } else {
+    return "Bend"
+  }
+}
 //User Interface
-//A variable is determined based on which radio input is selected. These variables are 1, 2, 10, 20, 100, 200, 1000, and 2000.
+//A value for each variable is determined based on which radio input is selected. These variables are 1, 2, 10, 20, 100, 200, 1000, and 2000.
 $(document).ready(function() {
   $("#buttonStart").click(function() {
     $(".landingQuestion").hide();
@@ -16,7 +37,6 @@ $(document).ready(function() {
       }
       $(".question#q1").hide();
       $(".question#q2").show();
-
   $(".question#q2").submit(function(event) {
     event.preventDefault();
     var q2Answer = $("input:radio[name=optradio2]:checked").val();
@@ -27,7 +47,6 @@ $(document).ready(function() {
       }
       $(".question#q2").hide();
       $(".question#q3").show();
-
   $(".question#q3").submit(function(event) {
     event.preventDefault();
     var q3Answer = $("input:radio[name=optradio3]:checked").val();
@@ -38,7 +57,6 @@ $(document).ready(function() {
       }
       $(".question#q3").hide();
       $(".question#q4").show();
-
   $(".question#q4").submit(function(event) {
     event.preventDefault();
     var q4Answer = $("input:radio[name=optradio4]:checked").val();
@@ -48,32 +66,8 @@ $(document).ready(function() {
         var ocean = 2000;
       }
       $(".question#q2").hide();
-
-//Business Logic
-//A string output is determined by adding up the relevant variables.
-      function vacationDecision (result) {
-
-        if (bigCities + introverted + singing===111) {
-          return "Lima"
-        } else if (bigCities + introverted + singing===211) {
-          return "Tokyo"
-        } else if (bigCities + introverted + ocean===1021) {
-          return "Rio de Janeiro"
-        } else if (bigCities + introverted + ocean===2021) {
-          return "Seattle"
-        } else if (bigCities + introverted + singing===112) {
-          return "Cusco"
-        } else if (bigCities + introverted + singing===212) {
-          return "Karuizawa"
-        } else if (bigCities + introverted + ocean===1022) {
-          return "Ilha Grande"
-        } else {
-          return "Bend"
-        }
-      }
-      var result;
       $(".question#q4").hide();
-      $(".result").html("You should go to "+ vacationDecision(result) + "!");
+      $(".result").html("You should go to "+ vacationDecision(bigCities, introverted, singing, ocean) + "!");
   });
   });
   });
---
+++ readme.md
@@ -0,0 +1,27 @@
+# _Vacation Quiz_
+
+#### _Four question quiz to determine an ideal vacation destination, Jan 20, 2018_
+
+#### By _**Mike Lambert**_
+
+## Description
+
+_This application asks 4 questions regarding personal preferences and suggests an ideal vacation spot. It was built as practice in basic javascript and jquery._
+
+## Setup/Installation Requirements
+
+* _Run on desktop browser_
+
+## Support and contact details
+
+_mlamb128@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS (bootstrap), Javascript, and jquery_
+
+### License
+
+*ML*
+
+Copyright (c) 2016 **_Michael B Lambert_**
